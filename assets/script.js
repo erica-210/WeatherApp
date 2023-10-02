bca67a734832e9dbe685c46a40f45d9ba4cce3c8
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-10-01 19:05:40 -0500

    added styling and tried to create search history feature

--- assets/script.js
+++ assets/script.js
@@ -39,54 +39,56 @@ var currentCity = document.querySelector('.current-city');
 
             document.getElementById('current-city').innerHTML =
                 `
-                <h2>${cityName}   Date:${data.list[0].dt_txt.substring(0, 10)}</h2>
-                <img style="width:50px; height:50px" src="${imageUrl}"/>
-                <li>Temperature: ${data.list[0].main.temp} </li>
-                <li>Wind Speed: ${data.list[0].wind.speed}</li>
-                <li>Humidity: ${data.list[0].main.humidity}</li>
+                <h2 style="text-transform: uppercase">${cityName}   <span style="color: #4267cea6">${data.list[0].dt_txt.substring(0, 10)}</span>
+                <img style="width:50px; height:50px" src="${imageUrl}"/></h2>
+                <li style="list-style:none; padding:10px; font-weight:bold">Temperature: ${data.list[0].main.temp} </li>
+                <li style="list-style:none; padding:10px; font-weight:bold">Wind Speed: ${data.list[0].wind.speed}</li>
+                <li style="list-style:none; padding:10px; font-weight:bold">Humidity: ${data.list[0].main.humidity}</li>
               `
             
              // when button is clicked future weather conditions are displayed in row
             // city name, date, icon or weather, temp, humidty, wind speed, uv index
-            for (let i = 7; i < data.list.length; i += 8) {
+            for (var i = 7; i < data.list.length; i += 8) {
 
-                let imageUrl2 = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
+                var imageUrl2 = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
 
                 document.getElementById('card').innerHTML +=
-                    `<h4>Date: ${data.list[i].dt_txt.substring(0, 10)}</h4>
-                    <img style="width:50px; height:50px" src="${imageUrl2}"/>
+                    `<h4>Date: ${data.list[i].dt_txt.substring(0, 10)}
+                    <img style="width:50px; height:50px" src="${imageUrl2}"/></h4>
                     <li>Temp: ${data.list[i].main.temp}</li>
                     <li>WindSpeed: ${data.list[i].wind.speed}</li>
                     <li>Humidity: ${data.list[i].main.humidity}</li>
                     `
+                    var individualCards = document.querySelector("#card");
+                    individualCards.setAttribute("style", "list-style:none; background-color:#4267cea6")
         }
-        });
 
         // save searched data in local storage
-        localStorage.setItem(cityName, data);
+        localStorage.setItem(cityName, JSON.stringify(data));
 
-        function searchHistory(){
-            var recentSearch = [];
-            recentSearch.push($('#search').val());
+        // when button is clicked city searched is added to search history
+        // when searched city is clicked under search history
+        // then logged city presents current and future condtions again
+        var history = JSON.parse(localStorage.getItem("cityName"));
+        
+        
+        function searchHistory() {
+            cityName = document.getElementById('search').value;
 
-            $.each(recentSearch, function(index, value){
-                document.getElementById('pastCities').innerHTML =
-                `
-                <button>${}
-                `
-                var list = document.createElement("p");
-                p.innerHTML = value;
-                document.getElementById('pastCities').appendChild(p);
-            })
+            for (i = 0; i < history.length; i++) {
+               //$(".pastCities").prepend("button type='button' class='btn btn-light prev-city'>" + history[i] + "</button>");
+                $(".pastCities").children('textarea').val(localStorage.getItem(history[i]));
+            }
         }
-        
-        
-    })
+        console.log(localStorage.getItem("history" + i));
+        searchHistory();
+     })
+
      .catch(function (error) {
         alert('ERROR');
       });
+     })
     }
-    
     // create search bar that cities can be typed into
     // allow for search button to be clicked 
     $('.btn').on('click', function(event) {
@@ -96,14 +98,7 @@ var currentCity = document.querySelector('.current-city');
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
         getApi(queryURL);
         console.log("He's ALIVE!");
-
-        
     })
 
-    // when button is clicked city searched is added to search history
-
-
-    // when searched city is clicked under search history
-    // then logged city presents current and future condtions again
 
 
